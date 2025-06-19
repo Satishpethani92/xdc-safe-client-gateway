44b5906724b89685b44e88c315d2f2f4eb6085a5
Satish pethani <satyapethani@gmail.com>
2025-06-19 12:03:22 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { IConfigurationService } from '@/config/configuration.service.interface'
 async function bootstrap(): Promise<void> {
   const app = await new DefaultAppProvider().provide(AppModule.register());
 
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://xdcsafe.xdcobserver.io'],
+  });
+
   const configurationService: IConfigurationService =
     app.get<IConfigurationService>(IConfigurationService);
   const applicationPort: string =
